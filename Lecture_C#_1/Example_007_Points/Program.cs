c5c2dd6498b918e1bdfe089b1446f153889df962
Vitaly <vit-krivenko@yandex.ru>
2022-11-28 21:19:51 +0300

    Задача с точками

--- Lecture_C#_1/Example_007_Points/Program.cs
+++ Lecture_C#_1/Example_007_Points/Program.cs
@@ -5,9 +5,9 @@
 int xa = 1;
 int ya = 1;
 int xb = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+int yb = 20;
+int xc = 30;
+int yc = 20;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -17,3 +17,20 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while (count<10)
+{
+    int what = new Random().Next(0,3);
+    if (what == 0)
+    {
+        x = (x +xa)/2;
+        y = (y+ yb)/2;
+    } 
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine("+");
+    count++;
+}
