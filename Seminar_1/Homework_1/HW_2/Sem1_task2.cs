3264dd97f9adaefd0f6663ada161b7d0df56e164
Vitaly <vit-krivenko@yandex.ru>
2022-12-01 18:12:35 +0300

    Решение задачи №2

---
+++ Seminar_1/Homework_1/HW_2/Sem1_task2.cs
@@ -0,0 +1,24 @@
+﻿string num1;
+Console.WriteLine("Введите первое число");
+num1 = Console.ReadLine()!;
+int parsednum1; 
+parsednum1 = int.Parse(num1);
+
+string num2;
+Console.WriteLine("Введите второе число");
+num2 = Console.ReadLine()!;
+int parsednum2; 
+parsednum2 = int.Parse(num2);
+
+if (parsednum1 > parsednum2)
+{
+    Console.WriteLine(parsednum1 + " большее число");
+}
+if (parsednum1 < parsednum2)
+{
+    Console.WriteLine(parsednum2 + " большее число");
+}
+if (parsednum1 == parsednum2)
+{
+    Console.WriteLine("Числа равны");
+}
