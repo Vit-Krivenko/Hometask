d62d8f4dfc473ae2461e25eec63d38de952a1f60
Vit-Krivenko <119007590+Vit-Krivenko@users.noreply.github.com>
2022-12-17 16:09:17 +0300

    Add files via upload

---
+++ Sem_6/Task_41.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите количество вводимых чисел");
+int M = int.Parse(Console.ReadLine()!);
+int[] array = new int[M];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите число");
+    array[i] = int.Parse(Console.ReadLine()!);
+}
+PrintArray(array);
+Console.WriteLine("Количество введеных чисел больше 0 : " + numberPlus(array));
+
+int numberPlus(int[] array)
+{
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            number++;
+        }
+    }
+    return number;
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
