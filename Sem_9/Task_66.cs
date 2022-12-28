39d26e8d3bf5aacbf3a99275fea5642a8761f5a4
Vit-Krivenko <119007590+Vit-Krivenko@users.noreply.github.com>
2022-12-28 20:49:37 +0300

    Add files via upload

---
+++ Sem_9/Task_66.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Сумма между m и n включительно составляет " + SummBetweenNumbers(m, n));
+
+
+int SummBetweenNumbers(int a, int b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    else
+    {
+        return a + SummBetweenNumbers(a + 1, b);
+    }
+}
---
+++ Sem_9/Task_68.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Функция Аккермана для m и n " + AkkermanFunction(m, n));
+
+
+
+int AkkermanFunction(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if ((a != 0) && (b == 0))
+    {
+        return AkkermanFunction(a - 1, 1);
+    }
+    else
+    {
+        return AkkermanFunction(a - 1, AkkermanFunction(a, b - 1));
+    }
+}
