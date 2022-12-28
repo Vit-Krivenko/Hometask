9f0ef10dbbd6a69cfde0873fa61d6ae817b341bc
Vit-Krivenko <119007590+Vit-Krivenko@users.noreply.github.com>
2022-12-28 20:49:01 +0300

    Add files via upload

---
+++ Sem_9/Task_64.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()!);
+PrintNumber(n);
+
+
+void PrintNumber(int n)
+{
+    if (n == 1)
+    {
+        Console.Write("1 ");
+        return;
+    }
+    Console.Write(n + ", ");
+    PrintNumber(n - 1);
+
+}
